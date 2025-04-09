c46c39dae246b8338f1fe216dd8d08fc65327b1f
mdabusaeed <abu.saeed.nicl@gmail.com>
2025-04-09 10:29:24 +0600

    add the filter

--- products/filters.py
+++ products/filters.py
@@ -1,11 +1,25 @@
 from products.models import Product
 from django_filters.rest_framework import FilterSet
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+
 
 class ProductFilterSet(FilterSet):
+    price_gt = filters.NumberFilter(field_name='price', lookup_expr='gt')
+    price_lt = filters.NumberFilter(field_name='price', lookup_expr='lt')
+
     class Meta:
         model = Product 
         fields = {
             'category_id': ['exact'],
-            'price': ['gt', 'lt'],
+            # 'price' is not needed here since we are defining price_gt and price_lt explicitly
         }
         
+
+# class ProductFilterSet(FilterSet):
+#     class Meta:
+#         model = Product 
+#         fields = {
+#             'category_id': ['exact'],
+#             'price': ['gt', 'lt'],
+#         }
