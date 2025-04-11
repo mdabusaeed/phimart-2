2a7777b21525860a3201eecf3ead9e1d4bbbe40e
mdabusaeed <abu.saeed.nicl@gmail.com>
2025-04-11 15:36:08 +0600

    update register activation

--- users/managers.py
+++ users/managers.py
@@ -5,6 +5,7 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
+        extra_fields.setdefault('is_superuser', False)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -14,7 +15,7 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password = None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
+        extra_fields.setdefault('is_active', True)
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True')
         if extra_fields.get('is_superuser') is not True:
